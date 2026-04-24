700d2fdebf15e9dd5f14c3dbe799db8fcf79e75c
yazanabdalrazak26-del <yazanabdalrazak26@gmail.com>
2026-04-24 22:08:51 +0300

    Profile

--- src/Profile/layout/Layout.tsx
+++ src/Profile/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 function Layout() {
